195b4eec19535ccd31083b243c45dce25696af28
Rigoberto Cepeda <rcepeda@ecomsur.com>
2023-06-12 17:54:47 -0600

    style(confing): :lipstick: Added next ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
   return (
     <>
       <h1>Hola mundo</h1>
+      <Button color="primary">clck me</Button>
     </>
   );
 }
