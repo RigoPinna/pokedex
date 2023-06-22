ff21eaa2c078e8aecc4d5f0050e4105b2afcb84f
Rigoberto Cepeda <rcepeda@ecomsur.com>
2023-06-21 18:43:00 -0600

    feat(components): :sparkles: Addend new styles to card and refactor code Card

--- components/pokemons/Card.tsx
+++ components/pokemons/Card.tsx
@@ -1,36 +1,53 @@
-import { FC } from "react";
-import {
-  Grid,
-  Image,
-  Row,
-  StyledCard,
-  StyledCardBody,
-  StyledCardFooter,
-  Text,
-} from "@nextui-org/react";
+import { FC, useLayoutEffect, useRef, useState } from "react";
+import { Grid, Image, Row, Card as UICard, Text } from "@nextui-org/react";
 
 import { _pokemons } from "@/interfaces";
 import { useRouter } from "next/router";
 
 interface _card extends _pokemons {}
+import styles from "./Card.module.css";
+import { useCard } from "@/hooks/useCard";
 
 export const Card: FC<_card> = ({ id, name, img }) => {
-  const router = useRouter();
-  const handleGoToPDP = () => router.push(`/pokemon/${id}`);
+  const ref = useRef<HTMLImageElement>(null);
+  const { background, isHover, handleGoToPDP, setIsHover } = useCard({
+    imgRef: ref,
+    id,
+  });
   return (
     <Grid xs={6} sm={3} md={2} xl={1}>
-      <StyledCard isHovered isPressable isPressed onClick={handleGoToPDP}>
-        <StyledCardBody css={{ p: 1 }}>
-          <Image src={img} css={{ width: "100%", height: 140 }} />
-          <StyledCardFooter>
+      <UICard
+        isHoverable
+        isPressable
+        onPress={handleGoToPDP}
+        css={{ overflow: "initial" }}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <UICard.Body
+          className={styles.__card_body}
+          css={{
+            p: 1,
+            overflow: "initial",
+            borderRadius: "10px",
+            background: isHover ? background.hovering : background.normal,
+          }}
+        >
+          <Image
+            ref={ref}
+            id={`pokemon_img_${id}`}
+            src={img}
+            css={{ width: "100%", height: 140 }}
+          />
+          <UICard.Footer className={styles.__card_footer}>
             <Row justify="space-between">
               <Text css={{ textTransform: "capitalize" }} h3>
                 {name}
               </Text>
             </Row>
-          </StyledCardFooter>
-        </StyledCardBody>
-      </StyledCard>
+          </UICard.Footer>
+        </UICard.Body>
+      </UICard>
     </Grid>
   );
 };
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -2,3 +2,4 @@ export * from "./pokemon-list";
 export * from "./home-page";
 export * from "./pdp-page";
 export * from "./pokemon-resp";
+export * from "./use-card";
